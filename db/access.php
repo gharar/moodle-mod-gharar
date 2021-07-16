71ac1fdf07263acff5e22c94fd7a34b09cf526c8
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-16 19:42:42 +0430

    Add a simple db/access.php file, for adding and viewing instances

---
+++ db/access.php
@@ -0,0 +1,29 @@
+<?php
+
+defined('MOODLE_INTERNAL') || exit();
+
+$capabilities = [
+    // Ability to add a new Gharar instance
+    'mod/gharar:add_instance' => [
+        'riskbitmask' => RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'manager' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+        ],
+        'clonepermissionsfrom' => 'moodle/course:manageactivities',
+    ],
+
+    // Ability to view instances, regardless of the type
+    'mod/gharar:view_instance' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => [
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ]
+    ],
+];
