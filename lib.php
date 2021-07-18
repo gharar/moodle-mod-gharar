ae3ba89a30aa83f1ebb5d939af29b59d470906e1
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-18 20:13:35 +0430

    Add gharar_*_instance() functions in lib.php

---
+++ lib.php
@@ -0,0 +1,38 @@
+<?php
+
+use mod_gharar\util;
+use mod_gharar\moodle_vars;
+
+util::forbid_access_if_not_from_moodle();
+
+function gharar_add_instance($ghararRecord)
+{
+    $moodle = moodle_vars::get_instance();
+
+    $id = $moodle->get_database()->insert_record('gharar', $ghararRecordData);
+
+    return $id;
+}
+
+function gharar_update_instance($ghararRecord)
+{
+    $moodle = moodle_vars::get_instance();
+
+    $id = $moodle->get_database()->update_record('gharar', $ghararRecordData);
+
+    return true;
+}
+
+function gharar_delete_instance($recordId)
+{
+    $moodle = moodle_vars::get_instance();
+
+    if (!$moodle->get_database()->get_record('gharar', ['id' => $recordId])) {
+        return false;
+    }
+
+    if (!$moodle->get_database()->delete_records('gharar', ['id' => $recordId])) {
+        $result = false;
+    }
+    return true;
+}
