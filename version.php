9112a3dd6dc286dcb2d0c8ca18aea5ba4003adba
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-19 01:56:59 +0430

    Add release information in version.php

--- version.php
+++ version.php
@@ -4,5 +4,6 @@ use \mod_gharar\util;
 
 util::forbid_access_if_not_from_moodle();
 
-$plugin->component = "mod_gharar";
+$plugin->component = 'mod_gharar';
 $plugin->version = 2021071601;
+$plugin->release = '0.1.0-alpha.1';
