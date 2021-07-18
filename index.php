3eec0a8b42ae38f72fe91bcf907abffa7c6b14d4
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-18 20:56:22 +0430

    Add lazy table view of the links of Gharar available in a course

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+use mod_gharar\util;
+use mod_gharar\moodle_vars;
+
 require_once __DIR__ . '/../../config.php';
 
 // Get id parameter, using GET (as in GET and POST) method
@@ -11,7 +14,7 @@ require_login($course);
 
 // TODO: Make these more dynamic, e.g. use get_string for set_title
 $PAGE->set_url('/mod/gharar/index.php', ['id' => $courseId]);
-$PAGE->set_title('Gharar');
+$PAGE->set_title(util::get_string('plugin_name_plural'));
 $PAGE->set_heading($course->fullname);
 $PAGE->set_cacheable(false);
 $PAGE->set_pagelayout('incourse');
@@ -20,5 +23,28 @@ $PAGE->navbar->add($PAGE->title, $PAGE->url);
 
 // TODO: Add a rendered to show all the meetings
 echo $OUTPUT->header();
-echo $OUTPUT->heading('Gharar meetings');
+echo $OUTPUT->heading(util::get_string('plugin_name_plural'));
+
+// TODO: Wrap it inside a renderable
+$table = new html_table();
+$table->attributes['class'] = 'generaltable mod_index';
+
+$table->head = [util::get_string('meeting_name'), util::get_string('meeting_link')];
+$table->align = ['center', 'center'];
+
+$instances = moodle_vars::get_instance()->get_database()->get_records('gharar', [
+    'course' => $courseId
+]);
+
+foreach ($instances as $instace) {
+    $row = [
+        html_writer::link(new moodle_url('view.php', ['id' => $courseId], $instance->name)),
+        $instance->link,
+    ];
+
+    $table->data[] = $row;
+}
+
+echo html_writer::table($table);
+
 echo $OUTPUT->footer();
