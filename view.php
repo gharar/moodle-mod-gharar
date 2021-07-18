d4df02f100251b0205e9a6f65c7985cf19251a17
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-18 21:07:16 +0430

    View Gharar meetings in view.php as a one-column table

--- view.php
+++ view.php
@@ -1,5 +1,7 @@
 <?php
 
+use mod_gharar\util;
+
 require_once __DIR__ . '/../../config.php';
 
 $courseId = required_param('id', PARAM_INT);
@@ -8,12 +10,32 @@ $course = $DB->get_record('course', ['id' => $courseId], '*', MUST_EXIST);
 
 require_login($course);
 
-// TODO:
 $PAGE->set_url('/mod/gharar/view.php', ['id' => $courseId]);
-$PAGE->set_title('Gharar meeting');
+$PAGE->set_title(util::get_string('plugin_name_plural'));
 $PAGE->set_heading($course->fullname);
 $PAGE->set_cacheable(false);
 
 echo $OUTPUT->header();
-echo $OUTPUT->heading('Gharar meeting');
+echo $OUTPUT->heading(util::get_string('plugin_name_plural'));
+
+$table = new html_table();
+$table->attributes['class'] = 'generaltable mod_index';
+
+$table->head = [util::get_string('meeting_name')];
+$table->align = ['center'];
+
+$instances = moodle_vars::get_instance()->get_database()->get_records('gharar', [
+    'course' => $courseId
+]);
+
+foreach ($instances as $instace) {
+    $row = [
+        html_writer::link($instance->name, $instance->link)
+    ];
+
+    $table->data[] = $row;
+}
+
+echo html_writer::table($table);
+
 echo $OUTPUT->footer();
