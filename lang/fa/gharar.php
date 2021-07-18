5b713215db8bcdb382e5689398b855ba3ca2a288
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-19 01:54:45 +0430

    Add pluginname language string (used by moodle)

--- lang/fa/gharar.php
+++ lang/fa/gharar.php
@@ -6,7 +6,9 @@ util::forbid_access_if_not_from_moodle();
 
 const PLUGIN_NAME = 'قرار';
 
+// Moodle-specific
 $string['modulename'] = PLUGIN_NAME;
+$string['pluginname'] = PLUGIN_NAME;
 
 $string['plugin_name'] = PLUGIN_NAME;
 $string['plugin_name_plural'] = PLUGIN_NAME . 'ها';
