2afaa053b445e3decaa8a34dbb698456f14eff51
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-19 15:22:26 +0430

    Make view.php title and heading showing instance name

--- view.php
+++ view.php
@@ -14,12 +14,12 @@ $instance = $DB->get_record('gharar', ['id'=> $courseModule->instance], '*', MUS
 require_login($course, true, $courseModule);
 
 $PAGE->set_url('/mod/gharar/view.php', ['id' => $courseModuleId]);
-$PAGE->set_title($instance->name);
+$PAGE->set_title("$course->shortname: $instance->name");
 $PAGE->set_heading($course->fullname);
 $PAGE->set_cacheable(false);
 
 echo $OUTPUT->header();
-echo $OUTPUT->heading(util::get_string('plugin_name_plural'));
+echo $OUTPUT->heading($instance->name);
 
 echo html_writer::link($instance->link, util::get_string('enter_meeting_link'));
 
