847882f5bc0c981b7e78bd53eeb3b9367a4f589f
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-19 15:24:56 +0430

    Remove the zip file before building in build-zip.sh
    Also, ignore the shell file from being included in the resulting
    zip file.

--- build-zip.sh
+++ build-zip.sh
@@ -13,8 +13,12 @@ mkdir "$tmpDir"
 cd "$tmpDir"
 
 # Make sure the resulting file includes only one directory matching the plugin's name
-rsync -av "$mainRepoDir/" "./$pluginName" --exclude 'git' --exclude '.gitignore' > /dev/null
+rsync -av "$mainRepoDir/" "./$pluginName" \
+    --exclude '.git' --exclude '.gitignore' --exclude 'build-zip.sh' > /dev/null
 
+# Remove previous zip file to prevent files to remain there
+# TODO: Remove this?
+rm "$mainRepoDir/$outputFilename"
 # Create the zip file in the current directory
 zip -r "$mainRepoDir/$outputFilename" "./$pluginName" > /dev/null
 
