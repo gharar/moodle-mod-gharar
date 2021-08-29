505da85b306b4b0205e62f6e46c53efa001cbcbb
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-08-29 21:25:35 +0430

    Add Globals::getPage(), add type hints for Globals class
    Type hints are being added supposing that the Moodle files presents
    somewhere in the project files, e.g. perhaps in vendor by a soft
    link to Moodle lib directory. This way, the autocompletion packages
    for IDEs and text-editors work great (as I tested, php-ide-serenata
    for Atom works perfect). Also, after adding Psalm static analyzer,
    there might be a method to escape from its errors.

--- src/Moodle/Globals.php
+++ src/Moodle/Globals.php
@@ -2,21 +2,34 @@
 
 namespace MAChitgarha\MoodleModGharar\Moodle;
 
+use moodle_page;
+use mysqli_native_moodle_database;
+use core_renderer;
+
+// For using $CFG global
+require_once __DIR__ . "/../../../../config.php";
+
 class Globals
 {
     private static $instance = null;
 
+    /** @var mysqli_native_moodle_database */
     private $database;
+    /** @var stdClass */
     private $config;
+    /** @var core_renderer */
     private $output;
+    /** @var moodle_page */
+    private $page;
 
     private function __construct()
     {
-        global $DB, $CFG, $OUTPUT;
+        global $DB, $CFG, $OUTPUT, $PAGE;
 
         $this->database = $DB;
         $this->config = $CFG;
         $this->output = $OUTPUT;
+        $this->page = $PAGE;
     }
 
     public static function getInstance()
@@ -27,18 +40,23 @@ class Globals
         return self::$instance;
     }
 
-    public function getDatabase()
+    public function getDatabase(): mysqli_native_moodle_database
     {
         return $this->database;
     }
 
-    public function getConfig()
+    public function getConfig(): stdClass
     {
         return $this->config;
     }
 
-    public function getOutput()
+    public function getOutput(): core_renderer
     {
         return $this->output;
+    }
+
+    public function getPage(): moodle_page
+    {
+        return $this->page;
     }
 }
