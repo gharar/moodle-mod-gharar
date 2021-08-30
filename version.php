dc545a830352a561fd631b14055e37db6aa6fe4c
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-08-30 12:21:39 +0430

    Use Plugin::COMPONENT_NAME in version.php, for $plugin->component

--- version.php
+++ version.php
@@ -3,9 +3,10 @@
 require __DIR__ . "/vendor/autoload.php";
 
 use MAChitgarha\MoodleModGharar\Util;
+use MAChitgarha\MoodleModGharar\Plugin;
 
 Util::forbidNonMoodleAccess();
 
-$plugin->component = 'mod_gharar';
+$plugin->component = Plugin::COMPONENT_NAME;
 $plugin->version = 2021071603;
 $plugin->release = '0.1.0-beta.2';
