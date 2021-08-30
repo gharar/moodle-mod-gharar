1078fd609444a377513b8534b11744e490f999e2
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-08-30 19:55:11 +0430

    Move build-zip.sh into bin/ directory, improve it
    Make its error handling better, improve path variables, and exclude
    the resulting zip from including the zip itself.

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -6,33 +6,43 @@ tmpDirname=".tmp"
 pluginName="gharar"
 outputFilename="moodle-mod-gharar.zip"
 
-mainRepoDir="$(pwd)"
+# Exit on error
+set -e
+
+mainRepoDir="$(dirname "$(realpath "$0")")/.."
 tmpDir="$mainRepoDir/../$tmpDirname"
+
+# The resulting file path
+zipFilePath="$mainRepoDir/$outputFilename"
 
 # Remove dev dependencies from vendor/ directory. Otherwise, the resulting file
 # will be huge in size. For example, 80KB versus 17MB for the directory alone.
 # At last, we will revert this change back.
-composer --no-dev install > /dev/null
+echo "Removing Composer dev dependencies..."
+composer --no-dev install > /dev/null 2>&1
 
-mkdir "$tmpDir"
+mkdir -p "$tmpDir"
 cd "$tmpDir"
 
 # Make sure the resulting file includes only one directory matching plugin's name
 rsync -av "$mainRepoDir/" "./$pluginName" \
-    --exclude ".git" --exclude ".gitignore" --exclude "build-zip.sh" > /dev/null
+    --exclude ".git" --exclude ".gitignore" --exclude "bin/" \
+    --exclude "$outputFilename" > /dev/null
 
-# Remove previous zip file to prevent files to remain there
-# TODO: Remove this?
-rm "$mainRepoDir/$outputFilename"
+# Remove previous zip file to prevent extra removed files to remain there
+if [[ -e "$zipFilePath" ]]; then
+    rm "$zipFilePath"
+fi
+
 # Create the zip file in the current directory
-zip -r "$mainRepoDir/$outputFilename" "./$pluginName" > /dev/null
+zip -r -y "$zipFilePath" "./$pluginName" > /dev/null
 
 # Cleanup
 cd "$mainRepoDir"
 rm -r "$tmpDir"
 
-# Revert Composer dev dependencies back
-cd "$mainRepoDir"
-composer install > /dev/null
-
 echo "Zip file created successfully."
+
+cd "$mainRepoDir"
+echo "Re-adding Composer dev dependencies..."
+composer install > /dev/null 2>&1
