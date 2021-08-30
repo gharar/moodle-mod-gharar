3ff8fdf2f9c8d57849ad8aeb93770e0ceadebeca
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-08-30 20:41:33 +0430

    Add modulenameplural string, used by Moodle

--- lang/en/gharar.php
+++ lang/en/gharar.php
@@ -7,13 +7,15 @@ use MAChitgarha\MoodleModGharar\Util;
 Util::forbidNonMoodleAccess();
 
 const PLUGIN_NAME = "Gharar";
+const PLUGIN_NAME_PLURAL = PLUGIN_NAME . "s";
 
 // Moodle-specific
 $string["modulename"] = PLUGIN_NAME;
 $string["pluginname"] = PLUGIN_NAME;
+$string["modulenameplural"] = PLUGIN_NAME_PLURAL;
 
 $string["plugin_name"] = PLUGIN_NAME;
-$string["plugin_name_plural"] = PLUGIN_NAME . "s";
+$string["plugin_name_plural"] = PLUGIN_NAME_PLURAL;
 
 $string["meeting_name"] = "Name";
 $string["meeting_link"] = "Link";
--- lang/fa/gharar.php
+++ lang/fa/gharar.php
@@ -7,13 +7,15 @@ use MAChitgarha\MoodleModGharar\Util;
 Util::forbidNonMoodleAccess();
 
 const PLUGIN_NAME = "قرار";
+const PLUGIN_NAME_PLURAL = PLUGIN_NAME . "ها";
 
 // Moodle-specific
 $string["modulename"] = PLUGIN_NAME;
 $string["pluginname"] = PLUGIN_NAME;
+$string["modulenameplural"] = PLUGIN_NAME_PLURAL;
 
 $string["plugin_name"] = PLUGIN_NAME;
-$string["plugin_name_plural"] = PLUGIN_NAME . "ها";
+$string["plugin_name_plural"] = PLUGIN_NAME_PLURAL;
 
 $string["meeting_name"] = "نام";
 $string["meeting_link"] = "پیوند";
