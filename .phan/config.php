cfe139c516bb7aec2572c0e94b4e39ff7c15d9aa
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-09-02 20:19:05 +0430

    Add Phan configuration in .phan/config.php

---
+++ .phan/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    "target_php_version" => "7.2",
+    "directory_list" => [
+        "src/",
+        "vendor/",
+    ],
+    "exclude_analysis_directory_list" => [
+        "vendor/",
+    ],
+    "suppress_issue_types" => [
+        "PhanUndeclaredConstant",
+    ],
+];
