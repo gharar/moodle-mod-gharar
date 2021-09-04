eeaec26f14c725b6652a56102dd4282fc4e53ed2
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-09-04 16:35:39 +0430

    Move access.php $capabilities contents to Plugin::CAPABILITIES

--- db/access.php
+++ db/access.php
@@ -3,31 +3,8 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use MAChitgarha\MoodleModGharar\Util;
+use MAChitgarha\MoodleModGharar\Plugin;
 
 Util::forbidNonMoodleAccess();
 
-$capabilities = [
-    // Ability to add a new Gharar instance
-    "mod/gharar:addinstance" => [
-        "riskbitmask" => RISK_XSS,
-        "captype" => "write",
-        "contextlevel" => CONTEXT_COURSE,
-        "archetypes" => [
-            "manager" => CAP_ALLOW,
-            "editingteacher" => CAP_ALLOW,
-        ],
-        "clonepermissionsfrom" => "moodle/course:manageactivities",
-    ],
-
-    // Ability to view instances, regardless of the type
-    "mod/gharar:view" => [
-        "captype" => "read",
-        "contextlevel" => CONTEXT_MODULE,
-        "archetypes" => [
-            "student" => CAP_ALLOW,
-            "teacher" => CAP_ALLOW,
-            "editingteacher" => CAP_ALLOW,
-            "manager" => CAP_ALLOW
-        ]
-    ],
-];
+$capabilities = Plugin::CAPABILITIES;
--- src/Plugin.php
+++ src/Plugin.php
@@ -4,9 +4,12 @@ namespace MAChitgarha\MoodleModGharar;
 
 class Plugin
 {
+    /** @var string */
     public const MODULE_NAME = "gharar";
+    /** @var string */
     public const COMPONENT_NAME = "mod_gharar";
 
+    /** @var string */
     public const DATABASE_MAIN_TABLE_NAME = "gharar";
 
     /**
@@ -15,4 +18,31 @@ class Plugin
      * @var string
      */
     public const RELATIVE_PATH = "/mod/" . self::MODULE_NAME;
+
+    /** @var string */
+    public const CAPABILITIES = [
+        // Ability to add a new Gharar instance
+        "mod/gharar:addinstance" => [
+            "captype" => "write",
+            "riskbitmask" => RISK_XSS,
+            "contextlevel" => CONTEXT_COURSE,
+            "archetypes" => [
+                "manager" => CAP_ALLOW,
+                "editingteacher" => CAP_ALLOW,
+            ],
+            "clonepermissionsfrom" => "moodle/course:manageactivities",
+        ],
+
+        // Ability to view instances, regardless of the type
+        "mod/gharar:view" => [
+            "captype" => "read",
+            "contextlevel" => CONTEXT_MODULE,
+            "archetypes" => [
+                "student" => CAP_ALLOW,
+                "teacher" => CAP_ALLOW,
+                "editingteacher" => CAP_ALLOW,
+                "manager" => CAP_ALLOW,
+            ],
+        ],
+    ];
 }
