bf14a2d6f699e99a1cb2ce716dda212c00c29d85
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-09-05 22:13:17 +0430

    Re-add English language support
    Reason: Without it, other languages does not work. Sad, but no way.

--- src/LanguageString/English.php
+++ src/LanguageString/English.php
@@ -3,9 +3,8 @@
 namespace MAChitgarha\MoodleModGharar\LanguageString;
 
 /**
- * As a Moodle plugin must have the English language string to be able to be
- * installed, this class exists. But, it is more like a stub file, and it is not
- * regularly updated (as English is not supported).
+ * Although the support for English language is not in priority, but it is
+ * needed for language strings of other languages (i.e. Persian here) to work.
  */
 class English
 {
@@ -17,6 +16,21 @@ class English
         "modulename" => self::PLUGIN_NAME,
         "pluginname" => self::PLUGIN_NAME,
         "modulenameplural" => self::PLUGIN_NAME_PLURAL,
+
+        "pluginadministration" => self::PLUGIN_NAME . " administration",
+
         // }
+
+        "plugin_name" => self::PLUGIN_NAME,
+        "plugin_name_plural" => self::PLUGIN_NAME_PLURAL,
+
+        "name" => "Name",
+        "access_token" => "Access token",
+        "room_name" => "Room name",
+        "is_private" => "Private",
+
+        "room_settings" => "Room Settings",
+
+        "enter_meeting_link" => "Enter",
     ];
 }
