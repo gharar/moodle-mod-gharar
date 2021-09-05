680cd8bd0e20f18fb8c3c22b98901b7b526f4144
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-09-05 16:31:41 +0430

    Add guideline about versioning in version.php

--- version.php
+++ version.php
@@ -6,6 +6,17 @@ use MAChitgarha\MoodleModGharar\Util;
 
 Util::forbidNonMoodleAccess();
 
+/*
+ * The release version follows semantic versioning, and the version number (i.e.
+ * in the form of YYYYMMDDXX) follows it, as described here.
+ *
+ * With every major or minor change (e.g. going from 0.1.0 to 0.2.0), the date
+ * part of the version number (i.e. YYYYMMDD) is changed. With a bug-fix
+ * release, the incremental part (i.e. XX) is then increased. It is highly
+ * unlikely to happen, but if it reaches its limit (i.e. 99) and we are going
+ * to have yet another bug-fix release, then we increase the minor part of the
+ * release, and update the version number as described.
+ */
 $plugin->component = "mod_gharar";
 $plugin->version = 2021071605;
 $plugin->release = "0.1.0";
