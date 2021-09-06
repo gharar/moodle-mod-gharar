dad932a33c8ec26db46289bf2f6f1160679535a2
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-09-06 05:13:34 +0430

    Fix AdminSettingsBuilder not having category and page

--- src/PageBuilding/AdminSettingsBuilder.php
+++ src/PageBuilding/AdminSettingsBuilder.php
@@ -3,33 +3,82 @@
 namespace MAChitgarha\MoodleModGharar\PageBuilding;
 
 use admin_root;
+use admin_category;
+use admin_settingpage;
 use admin_setting_configtext;
 use MAChitgarha\MoodleModGharar\Moodle\Globals;
 use MAChitgarha\MoodleModGharar\Util;
 
 class AdminSettingsBuilder
 {
+    private const CATEGORY_MAIN_NAME = "mod_gharar_main_settings_cat";
+    private const CATEGORY_MAIN_PARENT_NAME = "modsettings";
+
+    private const PAGE_MAIN_NAME = "mod_gharar";
+    private const PAGE_MAIN_PARENT_NAME = self::CATEGORY_MAIN_NAME;
+
     public const CONFIG_ACCESS_TOKEN_NAME = "mod_gharar/access_token";
 
     /**
      * The root of the admin settings.
      * @var admin_root
      */
-    private $settings;
+    private $root;
+
+    /** @var admin_settingpage */
+    private $mainPage;
 
     public function __construct()
     {
-        $this->settings = Globals::getInstance()->getAdminSettings();
+        $this->root = Globals::getInstance()->getAdminSettings();
     }
 
     public function build(): self
     {
-        if (!$this->settings->fulltree) {
-            return $this;
+        $this
+            ->buildMainCategory()
+            ->initMainPage();
+
+        if ($this->settings->fulltree) {
+            $this->addAccessTokenConfig();
         }
 
-        $this->addAccessTokenConfig();
+        $this->addMainPage();
 
+        return $this;
+    }
+
+    /**
+     * Category is a set of related settings.
+     */
+    private function buildMainCategory(): self
+    {
+        $mainCategory = new admin_category(
+            self::CATEGORY_MAIN_NAME,
+            Util::getString("plugin_name")
+        );
+        $this->root->add(
+            self::CATEGORY_MAIN_PARENT_NAME,
+            $mainCategory
+        );
+        return $this;
+    }
+
+    private function initMainPage(): self
+    {
+        $this->mainPage = new admin_settingpage(
+            self::PAGE_MAIN_NAME,
+            Util::getString("plugin_name")
+        );
+        return $this;
+    }
+
+    private function addMainPage(): self
+    {
+        $this->root->add(
+            self::PAGE_MAIN_PARENT_NAME,
+            $this->mainPage
+        );
         return $this;
     }
 
@@ -47,7 +96,7 @@ class AdminSettingsBuilder
             /* default: */ ""
         );
 
-        $this->settings->add($config);
+        $this->mainPage->add($config);
 
         return $this;
     }
