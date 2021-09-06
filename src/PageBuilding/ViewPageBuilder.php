066b08a1f077afc80ebedcbcb02cb00d21939edd
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-09-06 07:55:35 +0430

    Fix errors happens due to removal of stuff in ViewPageBuilder

--- src/PageBuilding/ViewPageBuilder.php
+++ src/PageBuilding/ViewPageBuilder.php
@@ -5,6 +5,7 @@ namespace MAChitgarha\MoodleModGharar\PageBuilding;
 use cm_info;
 use MAChitgarha\MoodleModGharar\Util;
 use MAChitgarha\MoodleModGharar\Plugin;
+use MAChitgarha\MoodleModGharar\Database;
 use MAChitgarha\MoodleModGharar\Moodle\Globals;
 use MAChitgarha\MoodleModGharar\GhararServiceAPI\Room\AvailableRoom;
 
@@ -13,44 +14,63 @@ class ViewPageBuilder extends AbstractPageBuilder
     public const URL = Plugin::RELATIVE_PATH . "/view.php";
 
     /** @var int */
-    private $moduleInstanceId;
+    private $instanceId;
 
     /** @var \stdClass */
     private $course;
     /** @var cm_info */
-    private $moduleInstance;
+    private $moduleInfo;
+
+    /** @var object */
+    private $instance;
 
     public function __construct()
     {
         $this
             ->initParams()
-            ->initCourseAndModuleInstance()
+            ->initCourseAndModuleInfo()
+            ->initInstance()
             ->requireLogin();
     }
 
     private function initParams(): self
     {
-        $this->moduleInstanceId = \required_param("id", \PARAM_INT);
+        $this->instanceId = \required_param("id", \PARAM_INT);
 
         return $this;
     }
 
-    private function initCourseAndModuleInstance(): self
+    private function initCourseAndModuleInfo(): self
     {
         [
             $this->course,
-            $this->moduleInstance
+            $this->moduleInfo
         ] = \get_course_and_cm_from_cmid(
-            $this->moduleInstanceId,
+            $this->instanceId,
             Plugin::MODULE_NAME
         );
 
         return $this;
     }
 
+    private function initInstance(): self
+    {
+        $this->instance = Globals::getInstance()
+            ->getDatabase()
+            ->get_record(
+                Database::TABLE_MAIN,
+                ["id" => $this->moduleInfo->instance],
+                "*",
+                \MUST_EXIST
+            );
+
+        return $this;
+    }
+
+
     private function requireLogin(): self
     {
-        \require_login($this->course, true, $this->moduleInstance);
+        \require_login($this->course, true, $this->moduleInfo);
 
         return $this;
     }
@@ -59,9 +79,9 @@ class ViewPageBuilder extends AbstractPageBuilder
     {
         $page = Globals::getInstance()->getPage();
 
-        $page->set_url(self::URL, ["id" => $this->moduleInstanceId]);
+        $page->set_url(self::URL, ["id" => $this->instanceId]);
         $page->set_title(
-            "{$this->course->shortname}: {$this->moduleInstance->name}"
+            "{$this->course->shortname}: {$this->instance->name}"
         );
         $page->set_heading($this->course->fullname);
         $page->set_cacheable(false);
@@ -71,14 +91,14 @@ class ViewPageBuilder extends AbstractPageBuilder
 
     protected function generateOutputHeading(): string
     {
-        return $this->moduleInstance->name;
+        return $this->instance->name;
     }
 
     protected function generateOutputMainContent(): string
     {
         $room = new AvailableRoom(
-            $this->moduleInstance->name,
-            $this->moduleInstance->address
+            $this->instance->name,
+            $this->instance->address
         );
 
         return \html_writer::link(
