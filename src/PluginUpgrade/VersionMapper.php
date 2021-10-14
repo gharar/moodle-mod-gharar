b9956641d44ddfc058001d14871244a0d57daf8f
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-10-14 20:12:09 +0330

    Update VersionMapper class

--- src/PluginUpgrade/VersionMapper.php
+++ src/PluginUpgrade/VersionMapper.php
@@ -10,11 +10,12 @@ class VersionMapper
     public const VERSION_UNKNOWN = -1;
 
     public const VERSION_0_1 = 20210716;
-    public const VERSION_0_2 = self::VERSION_UNKNOWN;
+    public const VERSION_0_2 = 20210905;
+    public const VERSION_0_3 = 20210924;
+    public const VERSION_0_4 = self::VERSION_UNKNOWN;
 
-    public static function extractDateFromVersionNumber(
-        int $version
-    ): int {
+    public static function extractDateFromVersionNumber(int $version): int
+    {
         return (int)($version / 100);
     }
 }
