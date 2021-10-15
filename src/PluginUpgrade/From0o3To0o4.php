a79d5bc69fb2cf6311b26af17d94fb0a5a89d4b6
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-10-15 06:23:29 +0330

    Add roles_can_view_recordings column to database
    Add upgrade support for it.

--- src/PluginUpgrade/From0o3To0o4.php
+++ src/PluginUpgrade/From0o3To0o4.php
@@ -18,16 +18,22 @@ class From0o3To0o4 extends AbstractBase
         self::FIELD_ATTR_DEFAULT => 1,
         self::FIELD_ATTR_SEQUENCE => false,
     ];
+    private const NEW_FIELD_ROLES_CAN_VIEW_RECORDINGS = [
+        self::FIELD_ATTR_NAME => "roles_can_view_recordings",
+        self::FIELD_ATTR_TYPE => \XMLDB_TYPE_TEXT,
+        self::FIELD_ATTR_NOT_NULL => true,
+        self::FIELD_ATTR_DEFAULT => "[]",
+        self::FIELD_ATTR_SEQUENCE => false,
+    ];
 
     protected const TABLE_MAIN_NEW_FIELDS = [
         self::NEW_FIELD_INTRO,
         self::NEW_FIELD_INTRO_FORMAT,
+        self::NEW_FIELD_ROLES_CAN_VIEW_RECORDINGS,
     ];
 
     protected function upgradeMainTableRecord(\stdClass $record): array
     {
-        $record->introformat = 1;
-
         return [true, $record];
     }
 }
