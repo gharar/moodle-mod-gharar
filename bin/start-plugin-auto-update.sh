20ce42a7e6b837a32314eca3d4edf8970ffcb878
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-11-04 12:05:46 +0330

    Exclude ZIP file from plugin-auto-update script

--- bin/start-plugin-auto-update.sh
+++ bin/start-plugin-auto-update.sh
@@ -16,7 +16,8 @@ trap "kill 0" EXIT
 pluginPath="$1"
 
 while inotifywait -r -e modify,create,delete,move .; do
-    rsync -a --delete --progress . "$pluginPath/" --exclude .git --exclude amd/build
+    rsync -a --delete --progress . "$pluginPath/" --exclude .git \
+        --exclude amd/build --exclude moodle-mod-gharar.zip
 done &
 
 while inotifywait -r -e modify,create,delete,move "$pluginPath/amd/build/"; do
